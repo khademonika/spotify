3db4025976a2450a397bcd3ab610ac5685ea2169
= <=>
2025-03-29 17:17:08 +0530

    Home page change

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className=" h-screen flex justify-center items-center overflow-y-hidden">
-      <h1 className="sm:text-4xl text-xl font-bold">Welcome to the Khebdya Shop (❁´◡`❁)</h1>
+      <h1 className="sm:text-4xl text-xl font-bold">Welcome to the Khebdya Shop 😒</h1>
     </div>
   );
 };
